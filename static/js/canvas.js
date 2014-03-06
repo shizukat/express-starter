db49f30df686cb11d0d717f34bc121ee13fba907
Shizuka Tiernan <10shizukat@berkeley.edu>
2014-03-05 19:22:10 -0800

    Yellow Blue Square!

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -18,4 +18,46 @@ $(document).ready(function() {
   $('#p2').click(function() {
   	context.strokeRect(10, 10, 100, 100)
   });
+
+  $('#p3').click(function() {
+  	context.beginPath();
+  	context.arc(100, 100, 100, 0, Math.PI/2)
+  	context.stroke();
+  });
+
+  $('#p4').click(function() {
+  	context.beginPath();
+  	context.arc(100, 100, 100, 0, 2*Math.PI)
+  	context.stroke();
+  });
+
+  $('#p5').click(function() {
+  	context.beginPath();
+  	context.moveTo(10, 10)
+  	context.lineTo(200, 200)
+  	context.closePath();
+  	context.stroke();
+  });
+
+  $('#p6').click(function() {
+  	context.strokeStyle='green'
+  	context.strokeRect(10, 10, 50, 80)
+  });
+
+  $('#p7').click(function() {
+  	context.strokeStyle='red'
+  	context.fillStyle='red'
+  	context.beginPath();
+  	context.arc(100, 100, 100, 0, 2*Math.PI)
+  	context.closePath();
+  	context.fill();
+  });
+
+  $('#p8').click(function() {
+  	context.strokeStyle='blue'
+  	context.fillStyle='yellow'
+  	context.strokeRect(10, 10, 200, 200)
+  	context.fillRect(10, 10, 200, 200)
+  });
+
 });
